3945d76621cc1525d484c96ddb68a66976c4e56b
mrmaximum80 <mrmaximum@mail.ru>
2023-09-06 03:07:33 +0300

    Add item requests fix codestyle #2

--- src/main/java/ru/practicum/shareit/request/map/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/map/ItemRequestMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request.map;
 
 import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.request.dto.ItemRequestDtoItems;
 
